077c0334b3f6b28b2502757b4028db49bd0d5fe3
𝑀𝑎𝑑ℎ𝑎𝑣 <madhavguglani@gmail.com>
2025-07-26 21:19:22 +0530

    Time: 41 ms (70.23%), Space: 45.5 MB (44.28%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> prefixSumCount;
+        prefixSumCount[0] = 1;  // base case: empty subarray has sum 0
 
-        unordered_map<int ,int > map;
-        long long maxlen =0;
-        int sum=0;
+        int count = 0;
+        int prefixSum = 0;
 
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
+        for (int i = 0; i < nums.size(); i++) {
+            prefixSum += nums[i];
 
-            if(sum==k){
-                maxlen = i+1;
+            // Check if there's a prefixSum that when removed gives sum = k
+            if (prefixSumCount.find(prefixSum - k) != prefixSumCount.end()) {
+                count += prefixSumCount[prefixSum - k];
             }
 
-
-            if(map.find(sum-k)!= map.end()){
-                int len = i - map[sum-k];
-                maxlen= max(len, maxlen);
-            }
-
-            if(map.find(sum)==map.end()){
-                map[sum]=i;
-            }
+            // Store the current prefixSum in the map
+            prefixSumCount[prefixSum]++;
         }
 
-        return maxlen;
-        
+        return count;
     }
 };
